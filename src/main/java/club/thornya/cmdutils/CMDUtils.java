308e4718e042c39e71bda709910ecc960d994598
Gusttavo <gustavosousa918@gmail.com>
2023-09-14 23:53:05 -0300

    initial commit

---
+++ src/main/java/club/thornya/cmdutils/CMDUtils.java
@@ -0,0 +1,17 @@
+package club.thornya.cmdutils;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CMDUtils extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
